f198f3026f153097afd3145bdc96500a4a07ceed
RealKwiebus <timveldkamp2@gmail.com>
2026-09-09 09:40:50 +0200

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype php>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+
+    <title>Welcome to adubbelman.veluwscollege.eu</title>
+
+
+
+  </head>
+
+  <body>
+  	<h2>Welkom op adubbelman.veluwscollege.eu</h2>
+<p>Je zit in het subdomain: adubbelman.veluwscollege.eu</p>
+  </body>
+</html>
